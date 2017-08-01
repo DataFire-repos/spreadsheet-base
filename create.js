2347aecbc1acc774e00161de5aa2c3106259566b
DataFire <info@datafire.io>
2017-08-01 12:53:05 -0400

    add to create action

--- create.js
+++ create.js
@@ -2,6 +2,9 @@
 let datafire = require('datafire');
 
 var google_sheets = require('@datafire/google_sheets').actions;
+
+const SHEET_ID = "15bv6kNNBkXp4SkuZ5eVh00E7nrEqHAu382aYp2sqUyA";
+
 module.exports = new datafire.Action({
   description: "Creates a new item in the spreadsheet",
   inputs: [{
@@ -16,7 +19,14 @@ module.exports = new datafire.Action({
     maximum: 200
   }],
   handler: (input, context) => {
+    console.log('this', Object.keys(this));
     return datafire.flow(context)
-      .then(_ => google_sheets.spreadsheets.values.update({}, context))
+      .then(_ => google_sheets.spreadsheets.values.append({
+        spreadsheetId: SHEET_ID,
+        range: "A1:A" + NUM_INPUTS,
+      }, context))
   },
 });
+
+const NUM_INPUTS = module.exports.inputs.length;
+console.log('inputs', NUM_INPUTS);
