c24c4da1adb1c3375b19eb4cc38464f96517176c
DataFire <info@datafire.io>
2017-08-02 13:23:49 -0400

    change page size

--- retrieve.js
+++ retrieve.js
@@ -7,7 +7,7 @@ function getColumnLetter(idx) {
   return String.fromCharCode(idx + 64);
 }
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 5;
 
 module.exports = new datafire.Action({
   description: "",
